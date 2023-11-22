eac224899c2f4f9dc34ee60eda53330fe88577d4
Rareshs <102982542+Rareshs@users.noreply.github.com>
2023-11-22 16:28:10 +0200

    Delete src directory

--- src/main/java/org/scrum/domain/project/ParsePDF.java
+++
@@ -1,7 +0,0 @@
-package org.scrum.domain.project;
-import com.groupdocs.parser.*;
-import com.groupdocs.parser.data.*;
-public class ParsePDF extends InvoiceTemplate {
-    Parser parser = new Parser("D://Invoices/invoice.pdf");
-    DocumentData data = parser.parseByTemplate(template);
-}
